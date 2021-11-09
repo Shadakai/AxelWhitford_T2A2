a7a8ffaed547fe136dd25a6cda2cc1a88b37045e
Axel Whitford <axelgw.7@gmail.com>
2021-11-09 15:47:03 +1000

    listings controller, database and view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :listings
+  devise_for :users, controllers: { registrarions: 'registrations' }
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
