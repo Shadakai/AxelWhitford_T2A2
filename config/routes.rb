832b358fb1b8ba39ebbe787f4fb113b295869b1a
Axel Whitford <axelgw.7@gmail.com>
2021-11-11 16:05:14 +1000

    listings/requests/profile controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :listings
+  get 'profile/index', to: 'profile#index', as: 'profile'
+  get 'requests/index', to: 'requests#index', as: 'requests'
+  get 'requests/new', to: 'requests#new', as: 'new_request'
+  get 'listings/index', to: 'listings#index', as: 'listings'
+  get 'listings/new', to: 'listings#new', as: 'new_listing'
   devise_for :users, controllers: { registrations: 'registrations' }
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
