9fb50df31022f34ac174ed5b4d4edd59d96ba258
Axel Whitford <axelgw.7@gmail.com>
2021-11-17 13:50:17 +1000

    Completion of shopping cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,27 @@
 class OrdersController < ApplicationController
-    def buy 
-        listing = Listing.find(params[:listing_id])
+    def index 
+        @listing = current_user.cart.listings
+    end
 
+    def buy 
         Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+        line_items = current_user.cart.listings.map do |listing|
+            {
+                price_data: {
+                  currency: 'aud',
+                  product_data: {
+                    name: listing.title,
+                  },
+                  unit_amount: listing.price * 100,
+                },
+                quantity: 1,
+              }
+        end
 
         session = Stripe::Checkout::Session.create({
             payment_method_types: ['card'],
-            line_items: [{
-              price_data: {
-                currency: 'aud',
-                product_data: {
-                  name: listing.title,
-                },
-                unit_amount: listing.price * 100,
-              },
-              quantity: 1,
-            }],
+            line_items: line_items,
             mode: 'payment',
             # These placeholder URLs will be replaced in a following step.
             success_url: success_url,
@@ -35,6 +41,12 @@ class OrdersController < ApplicationController
             listing = Listing.find(params[:listing_id])
             current_user.cart.listings << listing
             flash[:notice] = 'Book added to cart'
-            redirect_to listing_path
+            redirect_to listings_path
+        end
+
+        def buy_now
+            listing = Listing.find(params[:listing_id])
+            current_user.cart.listings << listing
+            buy
         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,17 @@ Rails.application.routes.draw do
   get 'profile/index', to: 'profile#index', as: 'profile'
 
   # Route for search bar
-  get '/search', to: 'listing#search'
+  get '/search', to: 'listings#search'
 
-  # Route to order
-  get '/buy/:listing_id', to: 'orders#buy', as: 'buy'
-  get 'add_to_cart/:listing_id', to: 'orders#add_to_cart', as: 'add_to_cart'
+  # Routes for single order
+  get '/buy', to: 'orders#buy', as: 'buy'
+  get '/buy_now/:listing_id', to: 'orders#buy_now', as: 'buy_now'
   get '/success', to: 'orders#success', as: 'success'
   get '/cancel', to: 'orders#cancel', as: 'cancel'
+
+  # Routes for cart
+  get 'add_to_cart/:listing_id', to: 'orders#add_to_cart', as: 'add_to_cart'
+  get '/cart', to: 'orders#index', as: 'cart'
 
   # Nest a comments route within the listings route example ~ Listing/1/comments
   resources :listings do 
