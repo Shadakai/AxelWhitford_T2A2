8e27eb3775dc3876a733f7757be457d5c20c3fd4
Axel Whitford <axelgw.7@gmail.com>
2021-11-24 09:17:20 +1000

    styling changes and seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Listing.destroy_all 
+
+User.create!(name: 'Axel Whitford', email: 'realemail@email.com', password: 'password',id: 1)
+
+Listing.create!(title: 'Weak Health Potion', description: 'A standard potion for healing your health.', price: 20 , user_id: 1)
+Listing.create!(title: 'Weak Mana Potion', description: 'A standard potion for restoring your depleted stamina.', price: 10 , user_id: 1)
+Listing.create!(title: 'Weak Stealth Potion', description: 'A standard potion for increasing your natural stealth abilities.', price: 30 , user_id: 1)
+Listing.create!(title: 'Weak Stamina Potion', description: 'A standard potion for healing your depleted stamina and endurance.', price: 15 , user_id: 1)
+Listing.create!(title: 'Weak Defence Potion', description: 'A standard potion for increasing your natural defensive abilities.', price: 40 , user_id: 1)
+Listing.create!(title: 'Weak Strength Potion', description: 'A standard potion for increasing you base level strength', price: 50 , user_id: 1)
+Listing.create!(title: 'Weak Regen Potion', description: 'A standard potion for raising your natural healing properties.', price: 25 , user_id: 1)
+Listing.create!(title: 'Weak Cold resistance Potion', description: 'A standard potion for increasing your resistance to the cold.', price: 35 , user_id: 1)
