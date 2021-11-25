36cc88844dfe4a850e28f01ba684120c8fe67a7b
Axel Whitford <axelgw.7@gmail.com>
2021-11-25 13:24:43 +1000

    js bug fix

--- app/assets/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-// Javascript needed for navbar 
-
-    const burgerIcon = document.querySelector('#burger');
-    const navbarMenu = document.querySelector('#nav-link');
-
-    burgerIcon.addEventListener('click', () => {
-      navbarMenu.classList.toggle('is-active');
-    });
